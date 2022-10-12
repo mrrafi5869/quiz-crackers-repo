ce2c368d4759bea187d2449bb5a4ce9eb6717bde
Mohammad Rafe <mrrafi5869@gmail.com>
2022-10-12 12:25:16 +0600

    question

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,18 +1,52 @@
 import React from 'react';
-import './Quiz.css'
+import './Quiz.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Quiz = ({quiz}) => {
     const {question, correctAnswer, options} = quiz;
-    console.log(options);
     // console.log(quiz);
+    const click = (option) => {
+        const correct = correctAnswer === option;
+        if(correct){
+            toast.success('correct!', {
+                position: "top-center",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+                });
+                
+        }
+        else{
+            toast.error('incorrect! please select a correct answer', {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+                });
+                
+        }
+    }
     return (
         <div className='quiz'>
             <h5 className='question'>{question}</h5>
             <div className='options'>
-                <input type="radio" name='options' id="" value={options[0]}/>{options[0]}
-                <input type="radio" name='options' id="" value={options[1]}/>{options[1]}
-                <input type="radio" name='options' id="" value={options[2]}/>{options[2]}
-                <input type="radio" name='options' id="" value={options[3]}/>{options[3]}
+                {
+                    options.map(option => 
+                        <div>
+                            <p><input type="radio" name='options' value={option} onClick = {() => click(option)}/>{option}</p>
+                            <ToastContainer></ToastContainer>
+                        </div>
+                        )
+                }
             </div>
         </div>
     );
--- src/components/Quizs/Quizs.jsx
+++ src/components/Quizs/Quizs.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Quiz from '../Quiz/Quiz';
+import './Quizs.css'
 
 const Quizs = () => {
     const quizs = useLoaderData().data.questions;
     return (
-        <div>
+        <div className='quizs container'>
             {
                 quizs.map(quiz => <Quiz
                     key={quiz.id}
