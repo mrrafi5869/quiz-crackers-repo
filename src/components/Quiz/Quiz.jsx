3821c54ed0c9c971ba70ad5f8f95f35130a5d580
Mohammad Rafe <mrrafi5869@gmail.com>
2022-10-12 13:52:14 +0600

    correct ans

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import './Quiz.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faQuestion } from '@fortawesome/free-solid-svg-icons'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Quiz = ({quiz}) => {
     const {question, correctAnswer, options} = quiz;
-    // console.log(quiz);
+    
+    const rightAnswer = () => {
+        toast.success(correctAnswer, {
+            position: "top-center",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            });
+    }
+
     const click = (option) => {
         const correct = correctAnswer === option;
         if(correct){
@@ -36,17 +51,22 @@ const Quiz = ({quiz}) => {
         }
     }
     return (
-        <div className='quiz'>
-            <h5 className='question'>{question}</h5>
-            <div className='options'>
-                {
-                    options.map(option => 
-                        <div>
-                            <p><input type="radio" name='options' value={option} onClick = {() => click(option)}/>{option}</p>
-                            <ToastContainer></ToastContainer>
-                        </div>
-                        )
-                }
+        <div>
+            <div className='quiz'>
+                <div className='d-flex align-items-center justify-content-between'>
+                    <h5 className='question'>{question.slice(3, -4)}</h5>
+                    <FontAwesomeIcon onClick={rightAnswer} icon={faQuestion}></FontAwesomeIcon>
+                </div>
+                <div className='options'>
+                    {
+                        options.map(option => 
+                            <div>
+                                <p onClick = {() => click(option)}><input type="radio" name='options' value={option}/>{option}</p>
+                                <ToastContainer></ToastContainer>
+                            </div>
+                            )
+                    }
+                </div>
             </div>
         </div>
     );
--- src/components/Quizs/Quizs.jsx
+++ src/components/Quizs/Quizs.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Quiz from '../Quiz/Quiz';
-import './Quizs.css'
+import './Quizs.css';
 
 const Quizs = () => {
+    const title = useLoaderData().data;
     const quizs = useLoaderData().data.questions;
     return (
-        <div className='quizs container'>
+        <div className='container'>
+            <h1 className='text-center mb-5'>{title.name}</h1>
+            <div className='quizs'>
             {
                 quizs.map(quiz => <Quiz
                     key={quiz.id}
                     quiz = {quiz}
+                    title = {title}
                 ></Quiz>)
             }
+        </div>
         </div>
     );
 };
